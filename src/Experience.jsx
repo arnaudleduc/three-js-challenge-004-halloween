35be7030fb3ad7d2c6dd41a10eab5476d3439fcb
Arnaud Leduc <arnaud.leduc3377@gmail.com>
2023-10-17 10:31:11 +0200

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,7 @@
+export default function Experience() {
+    return <>
+        <mesh>
+            <boxGeometry />
+        </mesh>
+    </>
+}
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import './style/main.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Canvas
+      camera={ {
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [ - 4, 3, 6 ]
+       } }
+    >
+      <Experience />
+    </Canvas>
+  </React.StrictMode>
+)
